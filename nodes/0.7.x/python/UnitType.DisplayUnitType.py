9be4dfcc017ac826013c92e1f711f1d7d2edc979
Andreas Dieckmann <dieckmann@caad.arch.rwth-aachen.de>
2015-06-03 11:22:41 +0200

    Version 0.75.34

--- nodes/0.7.x/python/UnitType.DisplayUnitType.py
+++ nodes/0.7.x/python/UnitType.DisplayUnitType.py
@@ -8,6 +8,11 @@ from RevitServices.Persistence import DocumentManager
 
 unittype = IN[0]
 doc = DocumentManager.Instance.CurrentDBDocument
-dispunits = doc.GetUnits().GetFormatOptions(unittype).DisplayUnits
-dispsym = LabelUtils.GetLabelFor(doc.GetUnits().GetFormatOptions(unittype).UnitSymbol)
+formatoptions = doc.GetUnits().GetFormatOptions(unittype)
+dispunits = formatoptions.DisplayUnits
+symtype = formatoptions.UnitSymbol
+if symtype == UnitSymbolType.UST_NONE:
+	dispsym = 'None'
+else:
+	dispsym = LabelUtils.GetLabelFor(symtype)
 OUT = (dispunits,dispsym)
