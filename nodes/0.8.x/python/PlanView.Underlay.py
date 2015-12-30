e5b62637c871c6f992257f39621a9b743cbdf749
Andreas Dieckmann <dieckmann@caad.arch.rwth-aachen.de>
2015-12-30 10:21:25 +0100

    Made PlanView.Underlay compatible with Revit 2016 R2 API

--- nodes/0.8.x/python/PlanView.Underlay.py
+++ nodes/0.8.x/python/PlanView.Underlay.py
@@ -6,11 +6,19 @@ views = UnwrapElement(IN[0])
 elementlist = list()
 
 for view in views:
+	# Revit 2016 R2 and later
 	try:
-		if view.get_Parameter(BuiltInParameter.VIEW_UNDERLAY_ID).AsElementId().IntegerValue == -1:
+		if view.GetUnderlayBaseLevel().IntegerValue == -1:
 			elementlist.append(list())
 		else:
-			elementlist.append(view.Document.GetElement(view.get_Parameter(BuiltInParameter.VIEW_UNDERLAY_ID).AsElementId()))
+			elementlist.append(view.Document.GetElement(view.GetUnderlayBaseLevel()))
+	# anything before
 	except:
-		elementlist.append(list())
+		try:
+			if view.get_Parameter(BuiltInParameter.VIEW_UNDERLAY_ID).AsElementId().IntegerValue == -1:
+				elementlist.append(list())
+			else:
+				elementlist.append(view.Document.GetElement(view.get_Parameter(BuiltInParameter.VIEW_UNDERLAY_ID).AsElementId()))
+		except:
+			elementlist.append(list())
 OUT = elementlist
