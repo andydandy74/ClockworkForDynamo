dcab13df824f4d9e9be329e8794bd6be4db28376
GMP-AD\adieckmann <adieckmann@gmp.de>
2023-11-11 21:38:00 +0100

    fixed #384

--- nodes/2.x/python/Element.SuperComponent.py
+++ nodes/2.x/python/Element.SuperComponent.py
@@ -3,15 +3,22 @@ clr.AddReference('RevitAPI')
 from Autodesk.Revit.DB import *
 
 def GetSuperComponent(item):
-	if hasattr(item, "SuperComponent"): 
-		sc = item.SuperComponent
-		if sc: return sc
-		else: return BeamSystem.BeamBelongsTo(item)
+	# nested families
+	if hasattr(item, "SuperComponent"): return item.SuperComponent
+	# beam systems
+	if hasattr(item, "BeamBelongsTo"): return BeamSystem.BeamBelongsTo(item)
+	# handrails
 	if hasattr(item, "HostRailingId"): return item.Document.GetElement(item.HostRailingId)
+	# stair components
 	elif hasattr(item, "GetStairs"): return item.GetStairs()
+	# site subregions
 	elif hasattr(item, "IsSiteSubRegion"):
 		if item.IsSiteSubRegion:
 			return item.Document.GetElement(item.AsSiteSubRegion().HostId)
+	# grid segments
+	elif item.GetType().ToString() == 'Autodesk.Revit.DB.Grid': 
+		sc = MultiSegmentGrid.GetMultiSegementGridId(item)
+		if sc: return item.Document.GetElement(sc)
 		else: return None
 	else: return None
 
