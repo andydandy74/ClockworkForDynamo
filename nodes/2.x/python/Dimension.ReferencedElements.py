226ff20788b4b99fa13b7a3837541c250b460ca8
Andreas Dieckmann <andreas@badmonkeys.net>
2024-03-30 21:36:40 +0100

    #403

--- nodes/2.x/python/Dimension.ReferencedElements.py
+++ nodes/2.x/python/Dimension.ReferencedElements.py
@@ -8,10 +8,24 @@ clr.ImportExtensions(Revit.Elements)
 
 def GetDimensionReferences(item):
 	if hasattr(item, "References"):
-		return [item.Document.GetElement(x.ElementId) for x in item.References]
+		refs = []
+		linkInstances = []
+		for r in item.References:
+			if r.LinkedElementId != ElementId.InvalidElementId: 
+				if item.Document.GetElement(r.ElementId).GetLinkDocument():
+					refs.append(item.Document.GetElement(r.ElementId).GetLinkDocument().GetElement(r.LinkedElementId))
+				# if the link isn't loaded, just return the RevitLinkInstance instead of the actual element
+				else: refs.append(item.Document.GetElement(r.ElementId))
+				linkInstances.append(item.Document.GetElement(r.ElementId))
+			else: 
+				refs.append(item.Document.GetElement(r.ElementId))
+				linkInstances.append(None)
+		return refs, linkInstances		
 	else: return None
 
 dimensions = UnwrapElement(IN[0])
 
-if isinstance(IN[0], list): OUT = [GetDimensionReferences(x) for x in dimensions]
+if isinstance(IN[0], list): 
+	if len(IN[0]) > 0: OUT = list(zip(*[GetDimensionReferences(x) for x in dimensions]))
+	else: OUT = [[],[]]
 else: OUT = GetDimensionReferences(dimensions)
