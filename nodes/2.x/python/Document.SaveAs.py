0439c8c2167f51ae3e34d7c9c430d65e58f48e4e
Andreas Dieckmann <andreas@badmonkeys.net>
2024-03-30 21:38:25 +0100

    #397

--- nodes/2.x/python/Document.SaveAs.py
+++ nodes/2.x/python/Document.SaveAs.py
@@ -1,6 +1,7 @@
 import clr
 clr.AddReference('RevitAPI')
 from Autodesk.Revit.DB import *
+import os
 
 clr.AddReference("RevitServices")
 import RevitServices
@@ -12,22 +13,27 @@ path = IN[0]
 compact = IN[1]
 newcentral = IN[2]
 isworkshared = IN[3]
+astemplate = IN[4]
 
 TransactionManager.Instance.ForceCloseTransaction()
 if doc.IsFamilyDocument:
 	path += '.rfa'
 else:
-	path += '.rvt'
+	if astemplate: path += '.rte'
+	else: path += '.rvt'
 opt = SaveAsOptions()
 opt.OverwriteExistingFile = True
 opt.Compact = compact
-if isworkshared and newcentral:
+if isworkshared and newcentral and not astemplate:
 	wsopt = WorksharingSaveAsOptions()
 	wsopt.ClearTransmitted = True
 	wsopt.SaveAsCentral = True
 	opt.SetWorksharingOptions(wsopt)
 try:
 	doc.SaveAs(path, opt)
+	if doc.IsFamilyDocument and astemplate: 
+		newpath = path.replace(".rfa", ".rft")
+		os.rename(path, newpath)
 	OUT = True
 except:
 	try:
