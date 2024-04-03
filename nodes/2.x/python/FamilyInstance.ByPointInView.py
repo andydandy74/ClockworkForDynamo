38b6815d95e3004c6d604a37273f0d323cba7c0e
Andreas Dieckmann <andreas@badmonkeys.net>
2024-04-03 13:11:48 +0200

    fixed #399 and one more for #179

--- nodes/2.x/python/FamilyInstance.ByPointInView.py
+++ nodes/2.x/python/FamilyInstance.ByPointInView.py
@@ -14,18 +14,30 @@ from RevitServices.Transactions import TransactionManager
 
 doc = DocumentManager.Instance.CurrentDBDocument
 points = UnwrapElement(IN[0])
-famtype = UnwrapElement(IN[1])
-lvl = UnwrapElement(IN[2])
-elementlist = list()
-counter = 0
+famtypes = UnwrapElement(IN[1])
+views = UnwrapElement(IN[2])
+
+def FamInstViewBased(point, ft, view):
+	# make sure familysymbol is active
+	if ft.IsActive == False:
+		ft.Activate()
+		doc.Regenerate()
+	newobj = doc.Create.NewFamilyInstance(point.ToXyz(),ft,view)
+	return newobj.ToDSType(False)
 
 TransactionManager.Instance.EnsureInTransaction(doc)
-# make sure familysymbol is active
-if famtype.IsActive == False:
-	famtype.Activate()
-	doc.Regenerate()
-for point in points:
-	newobj = doc.Create.NewFamilyInstance(point.ToXyz(),famtype,lvl)
-	elementlist.append(newobj.ToDSType(False))
+if isinstance(IN[0], list):
+	if isinstance(IN[1], list):
+		if isinstance(IN[2], list): OUT = [FamInstViewBased(x, y, z) for x, y, z in zip(points, famtypes, views)]
+		else: OUT = [FamInstViewBased(x, y, views) for x, y in zip(points, famtypes)]
+	else:
+		if isinstance(IN[2], list): OUT = [FamInstViewBased(x, famtypes, y) for x, y in zip(points, views)]
+		else: OUT = [FamInstViewBased(x, famtypes, views) for x in points]
+else:
+	if isinstance(IN[1], list):
+		if isinstance(IN[2], list): OUT = FamInstViewBased(points, famtypes[0], views[0])
+		else: OUT = FamInstViewBased(points, famtypes[0], views)
+	else:
+		if isinstance(IN[2], list): OUT = FamInstViewBased(points, famtypes, views[0])
+		else: OUT = FamInstViewBased(points, famtypes, views)
 TransactionManager.Instance.TransactionTaskDone()
-OUT = elementlist
