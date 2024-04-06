5e2be428067b7d404487fd4d957801447f0866de
Andreas Dieckmann <andreas@badmonkeys.net>
2024-04-06 10:48:22 +0200

    #205 #179

--- nodes/2.x/python/Element.CopyByVector.py
+++ nodes/2.x/python/Element.CopyByVector.py
@@ -16,23 +16,41 @@ from RevitServices.Transactions import TransactionManager
 
 doc = DocumentManager.Instance.CurrentDBDocument
 items = UnwrapElement(IN[0])
-xyz = UnwrapElement(IN[1]).ToXyz()
+if isinstance(IN[1], list): xyz = [UnwrapElement(x).ToXyz() for x in IN[1]]
+else: xyz = UnwrapElement(IN[1]).ToXyz()
 rehost = IN[2]
 
-ids = list()
-for item in items:
-	ids.append(item.Id)	
-itemlist = List[ElementId](ids)
-
 TransactionManager.Instance.EnsureInTransaction(doc)
-if rehost:
-	newitems = ElementTransformUtils.CopyElements(doc,itemlist,doc,Transform.CreateTranslation(xyz),None)
+if isinstance(IN[0], list): 
+	if isinstance(IN[1], list):
+		itemlist = [List[ElementId]([x.Id]) for x in items]
+		if rehost: 
+			newitems2 = [ElementTransformUtils.CopyElements(doc,x,doc,Transform.CreateTranslation(y),None) for x, y in zip(itemlist,xyz)]
+			newitems = [x[0] for x in newitems2]
+		else:
+			newitems2 = [ElementTransformUtils.CopyElements(doc,x,y) for x, y in zip(itemlist,xyz)]
+			newitems = [x[0] for x in newitems2]
+	else:
+		ids = [x.Id for x in items]
+		itemlist = List[ElementId](ids)
+		if rehost: newitems = ElementTransformUtils.CopyElements(doc,itemlist,doc,Transform.CreateTranslation(xyz),None)
+		else: newitems = ElementTransformUtils.CopyElements(doc,itemlist,xyz)
 else:
-	newitems = ElementTransformUtils.CopyElements(doc,itemlist,xyz)
+	if isinstance(IN[1], list):
+		itemlist1 = List[ElementId]([items.Id]) 
+		itemlist = []
+		[itemlist.append(itemlist1) for x in xyz]
+		if rehost: 
+			newitems2 = [ElementTransformUtils.CopyElements(doc,x,doc,Transform.CreateTranslation(y),None) for x, y in zip(itemlist,xyz)]
+			newitems = [x[0] for x in newitems2]
+		else:
+			newitems2 = [ElementTransformUtils.CopyElements(doc,x,y) for x, y in zip(itemlist,xyz)]
+			newitems = [x[0] for x in newitems2]
+	else:
+		itemlist = List[ElementId]([items.Id])
+		if rehost: newitems = ElementTransformUtils.CopyElements(doc,itemlist,doc,Transform.CreateTranslation(xyz),None)
+		else: newitems = ElementTransformUtils.CopyElements(doc,itemlist,xyz)
 TransactionManager.Instance.TransactionTaskDone()
 
-elementlist = list()
-for item in newitems:
-	elementlist.append(doc.GetElement(item).ToDSType(False))
-
-OUT = elementlist
+if isinstance(IN[0], list) or isinstance(IN[1], list): OUT = [doc.GetElement(x).ToDSType(False) for x in newitems]
+else: OUT = doc.GetElement(newitems[0]).ToDSType(False)
