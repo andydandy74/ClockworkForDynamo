30a72834da63854b5edd396d16cdfcf8c9ee3b47
Andreas Dieckmann <andreas@badmonkeys.net>
2024-05-04 14:34:15 +0200

    #179 Element.SetMaterialParameterByCategory

--- nodes/2.x/python/Element.SetMaterialParameterByCategory.py
+++ nodes/2.x/python/Element.SetMaterialParameterByCategory.py
@@ -8,24 +8,24 @@ from RevitServices.Persistence import DocumentManager
 from RevitServices.Transactions import TransactionManager
 
 doc = DocumentManager.Instance.CurrentDBDocument
-elements = UnwrapElement(IN[0])
-paramname = IN[1]
-booleans = list()
+items = UnwrapElement(IN[0])
+params = IN[1]
 
-TransactionManager.Instance.EnsureInTransaction(doc)
-for item in elements:
+def ResetMaterialParameter(item, param):
 	try:
 		paramset = False
-		for param in item.Parameters:
-			if param.Definition.Name == paramname:
-				param.Set(ElementId.InvalidElementId)
-				booleans.append(True)
-				paramset = True
-				break
-		if paramset == False:
-			booleans.append(False)
-	except:
-		booleans.append(False)
+		for p in item.Parameters:
+			if p.Definition.Name == param:
+				p.Set(ElementId.InvalidElementId)
+				return True
+		return False
+	except: return False
+		
+TransactionManager.Instance.EnsureInTransaction(doc)
+if isinstance(IN[0], list):
+	if isinstance(IN[1], list): OUT = [ResetMaterialParameter(x, y) for x, y in zip(items, params)]
+	else: OUT = [ResetMaterialParameter(x, params) for x in items]
+else:
+	if isinstance(IN[1], list): OUT = [ResetMaterialParameter(items, x) for x in params]
+	else: OUT = ResetMaterialParameter(items, params)
 TransactionManager.Instance.TransactionTaskDone()
-
-OUT = (elements,booleans)
