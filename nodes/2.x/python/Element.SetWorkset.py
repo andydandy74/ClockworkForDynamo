52394b4e389a3c07728a913cd4de86ee325d6070
Andreas Dieckmann <andreas@badmonkeys.net>
2024-05-04 13:11:45 +0200

    #179 Element.SetWorkset

--- nodes/2.x/python/Element.SetWorkset.py
+++ nodes/2.x/python/Element.SetWorkset.py
@@ -8,19 +8,25 @@ from RevitServices.Persistence import DocumentManager
 from RevitServices.Transactions import TransactionManager
 
 doc = DocumentManager.Instance.CurrentDBDocument
-faminsts = UnwrapElement(IN[0])
-ws = UnwrapElement(IN[1])
-# Is this an archi-lab workset?
-if isinstance(ws.Id, int): wsID = ws.Id
-else: wsID = ws.Id.IntegerValue
-booleans = list()
+items = UnwrapElement(IN[0])
+worksets = UnwrapElement(IN[1])
+
+def SetWorkset(item, ws):
+	if hasattr(ws, "Id"):
+		# Is this an archi-lab workset?
+		if isinstance(ws.Id, int): wsID = ws.Id
+		else: wsID = ws.Id.IntegerValue
+		try:
+			item.get_Parameter(BuiltInParameter.ELEM_PARTITION_PARAM).Set(wsID)
+			return True
+		except: return False
+	else: return False
+	
 TransactionManager.Instance.EnsureInTransaction(doc)
-for item in faminsts:
-	try:
-		param = item.get_Parameter(BuiltInParameter.ELEM_PARTITION_PARAM)
-		param.Set(wsID)
-		booleans.append(True)
-	except:
-		booleans.append(False)
+if isinstance(IN[0], list):
+	if isinstance(IN[1], list): OUT = [SetWorkset(x, y) for x, y in zip(items, worksets)]
+	else: OUT = [SetWorkset(x, worksets) for x in items]
+else:
+	if isinstance(IN[1], list): OUT = SetWorkset(items, worksets[0])
+	else: OUT = SetWorkset(items, worksets)
 TransactionManager.Instance.TransactionTaskDone()
-OUT = (faminsts,booleans)
