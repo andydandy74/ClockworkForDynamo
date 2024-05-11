9839a58b8d11f7d7bea2c510b4e2f19487188b28
Andreas Dieckmann <andreas@badmonkeys.net>
2024-05-11 07:51:15 +0200

    #179 RevitFaceReference.FromDynamoSurface

--- nodes/2.x/python/RevitFaceReference.FromDynamoSurface.py
+++ nodes/2.x/python/RevitFaceReference.FromDynamoSurface.py
@@ -2,12 +2,20 @@ import clr
 clr.AddReference('RevitAPI')
 from Autodesk.Revit.DB import *
 
-faces = UnwrapElement(IN[0])
-elementlist = list()
-for face in faces:
-	try:
+clr.AddReference("RevitServices")
+import RevitServices
+from RevitServices.Persistence import DocumentManager
+
+def GetRevitFaceRef(face):
+	try: 
 		ref = face.Tags.LookupTag("RevitFaceReference")
-		elementlist.append(ref)
-	except:
-		elementlist.append(list())
-OUT = elementlist
+		try: stableref = ref.ConvertToStableRepresentation(doc)
+		except: stableref = None
+		return ref, stableref
+	except: return None, None
+
+doc = DocumentManager.Instance.CurrentDBDocument
+faces = UnwrapElement(IN[0])
+
+if isinstance(IN[0], list): OUT = list(zip(*[GetRevitFaceRef(x) for x in faces]))
+else: OUT = GetRevitFaceRef(faces)
