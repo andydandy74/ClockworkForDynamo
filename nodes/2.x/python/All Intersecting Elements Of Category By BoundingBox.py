7f45765b5aa1db991408121a0aec03f074e8ca3a
Andreas Dieckmann <andreas@badmonkeys.net>
2024-10-05 13:00:58 +0200

    fixed #386 fixed #179 fixed #370

--- nodes/2.x/python/All Intersecting Elements Of Category By BoundingBox.py
+++ nodes/2.x/python/All Intersecting Elements Of Category By BoundingBox.py
@@ -15,24 +15,38 @@ clr.AddReference("RevitServices")
 import RevitServices
 from RevitServices.Persistence import DocumentManager
 
-doc = DocumentManager.Instance.CurrentDBDocument
-cutters = UnwrapElement(IN[0])
-view = UnwrapElement(IN[1])
-tol = IN[2]
-filtercats = List[ElementId]([x.Id for x in IN[3]])
-catfilter = ElementMulticategoryFilter(filtercats)
+def DisplayUnitToInternalUnit(val, unittype):
+	formatoptions = doc.GetUnits().GetFormatOptions(unittype)
+	if version > 2021: dispunits = formatoptions.GetUnitTypeId()
+	else: dispunits = formatoptions.DisplayUnits
+	try: return UnitUtils.ConvertToInternalUnits(val,dispunits)
+	except: return None
 
-intersectorlist = list()
-for cutter in cutters:
-	if cutter is None: intersectorlist.append([])
+def FindIntersects(item):
+	if item is None: return []
 	else:
-		bbox = cutter.get_BoundingBox(view)
-		bboxfilter = BoundingBoxIntersectsFilter(Outline(bbox.Min,bbox.Max),tol)
-		collector = FilteredElementCollector(doc, view.Id)
+		if view:
+			bbox = item.get_BoundingBox(view)
+			collector = FilteredElementCollector(doc, view.Id)
+		else:
+			bbox = item.get_BoundingBox(None)
+			collector = FilteredElementCollector(doc)
+		bboxfilter = BoundingBoxIntersectsFilter(Outline(bbox.Min,bbox.Max),tol)		
 		excludelist = []
-		excludelist.append(cutter.Id)
+		excludelist.append(item.Id)
 		excludeIDs = List[ElementId](excludelist)
 		excfilter = ExclusionFilter(excludeIDs)
-		intersectorlist.append(collector.WherePasses(bboxfilter).WherePasses(excfilter).WherePasses(catfilter).ToElements())
-	
-OUT = intersectorlist
+		return collector.WherePasses(bboxfilter).WherePasses(excfilter).WherePasses(catfilter).ToElements()
+
+doc = DocumentManager.Instance.CurrentDBDocument
+items = UnwrapElement(IN[0])
+view = UnwrapElement(IN[1])
+version = IN[4]
+filtercats = List[ElementId]([x.Id for x in IN[3]])
+catfilter = ElementMulticategoryFilter(filtercats)
+if version > 2021: unittype = ForgeTypeId('autodesk.spec.aec:length-2.0.0')
+else: unittype = UnitType.UT_Length
+tol = DisplayUnitToInternalUnit(IN[2], unittype)
+
+if isinstance(IN[0], list): OUT = [FindIntersects(x) for x in items]
+else: OUT = FindIntersects(items)
