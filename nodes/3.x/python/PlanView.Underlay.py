84cde70e3b361c027288bb5d100544be17508e81
Andreas Dieckmann <andreas@badmonkeys.net>
2024-10-12 20:22:38 +0200

    #416 #417

--- nodes/3.x/python/PlanView.Underlay.py
+++ nodes/3.x/python/PlanView.Underlay.py
@@ -1,21 +1,16 @@
 import clr
 clr.AddReference('RevitAPI')
 from Autodesk.Revit.DB import *
+import System
 
 def GetUnderlays(view):
 	baselvl = None
 	toplvl = None
 	ulOrientation = None
-	# Revit 2016 R2 and later
 	if hasattr(view, "GetUnderlayBaseLevel"):	
 		if view.GetUnderlayBaseLevel().IntegerValue > 0: baselvl = view.Document.GetElement(view.GetUnderlayBaseLevel())
 		if view.GetUnderlayTopLevel().IntegerValue > 0: toplvl = view.Document.GetElement(view.GetUnderlayTopLevel())
-		ulOrientation = str(view.GetUnderlayOrientation())
-	# anything before
-	else:
-		try: 
-			if view.get_Parameter(BuiltInParameter.VIEW_UNDERLAY_ID).AsElementId().IntegerValue > 0: baselvl = view.Document.GetElement(view.get_Parameter(BuiltInParameter.VIEW_UNDERLAY_ID).AsElementId())
-		except: pass
+		ulOrientation = System.Enum.GetName(UnderlayOrientation, view.GetUnderlayOrientation())
 	return baselvl, toplvl, ulOrientation
 
 views = UnwrapElement(IN[0])
