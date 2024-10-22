4c8dd81e22fafa7dd9b4fc056ab11ec3739759db
Andreas Dieckmann <andreas@badmonkeys.net>
2024-10-22 22:43:03 +0200

    #417

--- nodes/3.x/python/FamilyParameter.GetValueByFamilyType.py
+++ nodes/3.x/python/FamilyParameter.GetValueByFamilyType.py
@@ -10,17 +10,12 @@ doc = DocumentManager.Instance.CurrentDBDocument
 def GetFamParamVal(ft, fp):
 	if fp.StorageType == StorageType.Integer: return ft.AsInteger(fp)
 	elif fp.StorageType == StorageType.Double: 
-		if version > 2021:
-			if fp.GetUnitTypeId() and ft.HasValue(fp): return UnitUtils.ConvertFromInternalUnits(ft.AsDouble(fp),fp.GetUnitTypeId())
-			else: return ft.AsDouble(fp)
-		else:
-			if fp.DisplayUnitType and ft.HasValue(fp): return UnitUtils.ConvertFromInternalUnits(ft.AsDouble(fp),fp.DisplayUnitType)
-			else: return ft.AsDouble(fp)
+		if fp.GetUnitTypeId() and ft.HasValue(fp): return UnitUtils.ConvertFromInternalUnits(ft.AsDouble(fp),fp.GetUnitTypeId())
+		else: return ft.AsDouble(fp)
 	elif fp.StorageType == StorageType.String: return ft.AsString(fp)
 	elif fp.StorageType == StorageType.ElementId: return doc.GetElement(ft.AsElementId(fp))
 	else: return None
 
-version = IN[2]
 if isinstance(IN[0], list): 
 	if isinstance(IN[1], list): 
 		OUT = []
