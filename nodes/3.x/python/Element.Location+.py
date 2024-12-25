366a6298d4a0577f8e18a7684c3fe704c780bb91
Andreas Dieckmann <andreas@badmonkeys.net>
2024-12-25 12:15:08 +0100

    #437 3.x changes

--- nodes/3.x/python/Element.Location+.py
+++ nodes/3.x/python/Element.Location+.py
@@ -26,7 +26,6 @@ def GetLocation(item):
 	# [6] has rotation?
 	# [7] is curve loop?
 	# [8] curve loop
-	
 	# default values
 	point = None
 	curveendpoints = None
@@ -40,7 +39,6 @@ def GetLocation(item):
 	curveloop = None
 	# template for return statements:
 	# return point, curveendpoints, curve, ispoint, iscurve, haslocation, rotationangle, hasrotation, iscurveloop, curveloop
-	
 	# points and text notes
 	if hasattr(item, "Coord"): 
 		return item.Coord.ToPoint(), curveendpoints, curve, True, iscurve, True, rotationangle, hasrotation, iscurveloop, curveloop
@@ -70,11 +68,16 @@ def GetLocation(item):
 		return point, GetCurvePoints(item), item.ToProtoType(), ispoint, True, True, rotationangle, hasrotation, iscurveloop, curveloop
 	# railings and top rails
 	elif hasattr(item, "GetPath"):
-		railpath = [x.ToProtoType() for x in item.GetPath()]
+		railpath = []
+		for x in item.GetPath():
+			try: railpath.append(x.ToProtoType())
+			except: pass
 		# paths with multiple segments
 		if len(railpath) > 1: return point, curveendpoints, curve, ispoint, iscurve, True, rotationangle, hasrotation, True, railpath
 		# paths with a single segment
-		else: return point, GetCurvePoints(railpath[0].ToRevitType()), railpath[0], ispoint, True, True, rotationangle, hasrotation, iscurveloop, curveloop
+		elif len(railpath) == 1: return point, GetCurvePoints(railpath[0].ToRevitType()), railpath[0], ispoint, True, True, rotationangle, hasrotation, iscurveloop, curveloop
+		# paths without legible segments
+		else: return point, curveendpoints, curve, ispoint, iscurve, haslocation, rotationangle, hasrotation, iscurveloop, curveloop
 	# openings
 	elif hasattr(item, "BoundaryCurves"):
 		# rectangular openings
@@ -95,10 +98,13 @@ def GetLocation(item):
 		return point, curveendpoints, curve, ispoint, iscurve, True, rotationangle, hasrotation, True, sketchloop
 	# newer sketch-based elements (except walls)
 	elif hasattr(item, "SketchId") and not hasattr(item, "WallType"):
-		sketchloop = []
-		for loop in item.Document.GetElement(item.SketchId).Profile:
-			sketchloop.append([x.ToProtoType() for x in loop])
-		return point, curveendpoints, curve, ispoint, iscurve, True, rotationangle, hasrotation, True, sketchloop
+		sketch = item.Document.GetElement(item.SketchId)
+		if sketch:
+			sketchloop = []
+			for loop in sketch.Profile:
+				sketchloop.append([x.ToProtoType() for x in loop])
+			return point, curveendpoints, curve, ispoint, iscurve, True, rotationangle, hasrotation, True, sketchloop
+		else: return point, curveendpoints, curve, ispoint, iscurve, haslocation, rotationangle, hasrotation, iscurveloop, curveloop
 	# other elements
 	elif hasattr(item, "Location"):
 		loc = item.Location
@@ -111,9 +117,11 @@ def GetLocation(item):
 				except: return GetCurvePoints(loc.Curve)[0], curveendpoints, curve, True, iscurve, True, rotationangle, hasrotation, iscurveloop, curveloop
 			# point-based elements (e.g. most loadable families)
 			elif loc.ToString() == 'Autodesk.Revit.DB.LocationPoint':
-				if hasattr(loc, "Rotation"):
-					rotationangle = math.degrees(loc.Rotation)
-					hasrotation = True
+				try:
+					if hasattr(loc, "Rotation"):
+						rotationangle = math.degrees(loc.Rotation)
+						hasrotation = True
+				except: pass
 				return loc.Point.ToPoint(), curveendpoints, curve, True, iscurve, True, rotationangle, hasrotation, iscurveloop, curveloop
 			# some elements have a location property but don't return curves or points
 			else:
