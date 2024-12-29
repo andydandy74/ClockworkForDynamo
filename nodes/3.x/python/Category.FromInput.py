e3bf7e6fd6cb739d57c3ddb19cd93be5f936ff94
Andreas Dieckmann <andreas@badmonkeys.net>
2024-12-29 10:21:45 +0100

    #416

--- nodes/3.x/python/Category.FromInput.py
+++ nodes/3.x/python/Category.FromInput.py
@@ -13,17 +13,18 @@ from RevitServices.Persistence import DocumentManager
 
 def GetCatFromInput(var):	
 	if var:
-		cattype = var.GetType().ToString()
-		if cattype == "Autodesk.Revit.DB.Category": return var
-		elif cattype == "Autodesk.Revit.DB.BuiltInCategory": return Category.GetCategory(doc, var)
-		elif cattype == "System.String": 
-			found = [x for x in bics if x.ToString() == var]
-			if len(found) > 0: return Category.GetCategory(doc, found[0])	
-		else: return None			
+		if isinstance(var, str): 
+			try: return Category.GetCategory(doc, eval("BuiltInCategory." + var))
+			except: return None
+		elif isinstance(var, int): return Category.GetCategory(doc, var)
+		else:
+			cattype = var.GetType().ToString()
+			if cattype == "Autodesk.Revit.DB.Category": return var
+			elif cattype == "Autodesk.Revit.DB.BuiltInCategory": return Category.GetCategory(doc, var)
+			else: return None
 	else: return None
 
 doc = DocumentManager.Instance.CurrentDBDocument
-bics = System.Enum.GetValues(BuiltInCategory)
 cats = UnwrapElement(IN[0])
 
 if isinstance(IN[0], list): OUT = [GetCatFromInput(x) for x in cats]
