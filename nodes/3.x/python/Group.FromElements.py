5418c5decf5f81dcb9b2c855c436590b23de97b2
Andreas Dieckmann <andreas@badmonkeys.net>
2025-03-16 21:37:54 +0100

    fixed #452 (which was caused by #415)

--- nodes/3.x/python/Group.FromElements.py
+++ nodes/3.x/python/Group.FromElements.py
@@ -9,20 +9,21 @@ from RevitServices.Persistence import DocumentManager
 from RevitServices.Transactions import TransactionManager
 
 def MakeNewGroup(items, name):
-	ids = []
-	rejects = []
-	for item in items:
-		if item.Document.GetElement(item.GroupId): rejects.append(item)
-		else:
-			try: ids.append(item.Id)
-			except: rejects.append(item)
-	itemsNew = List[ElementId](ids)
-	try:
-		group = doc.Create.NewGroup(itemsNew)
-		group.GroupType.Name = name
-	except: 
-		group = None
-		rejects = items
+    ids = []
+    rejects = []
+    for item in items:
+        if item.Document.GetElement(item.GroupId): rejects.append(item)
+        else:
+            try: ids.append(item.Id)
+            except: rejects.append(item)
+    itemsNew = List[ElementId](ids)
+    try:
+        group = doc.Create.NewGroup(itemsNew)
+        group.GroupType.Name = name
+    except: 
+        group = None
+        rejects = items
+    return group, rejects
 
 doc = DocumentManager.Instance.CurrentDBDocument
 items = UnwrapElement(IN[0])
@@ -30,7 +31,7 @@ names = IN[1]
 
 TransactionManager.Instance.EnsureInTransaction(doc)
 if isinstance(IN[1], list):
-	if isinstance(IN[0], list): OUT = list(zip(*[MakeNewGroup(x, y) for x, y in zip (items, names)]))
-	else: OUT = MakeNewGroup(items, names[0])
+    if isinstance(IN[0], list): OUT = list(zip(*[MakeNewGroup(x, y) for x, y in zip (items, names)]))
+    else: OUT = MakeNewGroup(items, names[0])
 else: OUT = MakeNewGroup(items, names)
 TransactionManager.Instance.TransactionTaskDone()
