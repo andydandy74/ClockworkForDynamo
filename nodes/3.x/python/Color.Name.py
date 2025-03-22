404ec452581340527800337c278ed3b9d2ee684f
Andreas Dieckmann <andreas@badmonkeys.net>
2025-03-22 07:29:57 +0100

    #448

--- nodes/3.x/python/Color.Name.py
+++ nodes/3.x/python/Color.Name.py
@@ -3,14 +3,18 @@ clr.AddReference('System.Drawing')
 import System.Drawing
 
 def GetColorName(color):
-	if hasattr(color, "Red"):
-		if color.Red == 0 and color.Green == 0 and color.Blue == 0: return "Black"
-		else:
-			lookup = [x for x in knowncols if x.R == color.Red and x.G == color.Green and x.B == color.Blue]
-			if len(lookup) > 0: return lookup[0].Name
-			else: return None
-	else: return None
+    if hasattr(color, "Red"):
+        lookup = [x for x in syscols if (x[1] == color.Red and x[2] == color.Green and x[3] == color.Blue)]
+        if len(lookup) > 0: return lookup[0][0]
+        else: return None
+    else: return None
 
-knowncols = [System.Drawing.Color.FromKnownColor(x) for x in System.Enum.GetValues(System.Drawing.KnownColor)]
+syscols = []
+syscols.append(("Black",0,0,0))
+syscols.append(("White",255,255,255))
+potentialcols = dir(System.Drawing.KnownColor)
+for pc in potentialcols:
+    c = System.Drawing.Color.FromName(pc)
+    if c.R > 0 or c.G > 0 or c.B > 0: syscols.append((pc,c.R,c.G,c.B))
 if isinstance(IN[0], list): OUT = [GetColorName(x) for x in IN[0]]
 else: OUT = GetColorName(IN[0])
