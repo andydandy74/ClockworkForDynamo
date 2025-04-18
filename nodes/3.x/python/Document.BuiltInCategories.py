9c6e56c32ac2e788a87b52491df2e02d02a6ffdf
Andreas Dieckmann <andreas@badmonkeys.net>
2025-04-19 00:09:42 +0200

    fixed #459

--- nodes/3.x/python/Document.BuiltInCategories.py
+++ nodes/3.x/python/Document.BuiltInCategories.py
@@ -8,10 +8,13 @@ clr.AddReference("RevitNodes")
 import Revit
 
 dynamoCatsOnly = IN[0]
+version = IN[1]
 biclist = [eval("BuiltInCategory."+x) for x in dir(BuiltInCategory) if x.startswith("OST_")]
 cdata = []
 for bic in biclist:
-	try: cdata.append((System.Enum.GetName(BuiltInCategory, bic), ElementId(bic), Revit.Elements.Category.ById(ElementId(bic).IntegerValue)))
-	except:
-		if not dynamoCatsOnly: cdata.append((bic, ElementId(bic), None))
+    try: 
+        if version < 2025: cdata.append((System.Enum.GetName(BuiltInCategory, bic), ElementId(bic), Revit.Elements.Category.ById(ElementId(bic).IntegerValue)))
+        else: cdata.append((System.Enum.GetName(BuiltInCategory, bic), ElementId(bic), Revit.Elements.Category.ById(ElementId(bic).Value)))
+    except:
+        if not dynamoCatsOnly: cdata.append((bic, ElementId(bic), None))
 OUT = map(list, zip(*cdata))
