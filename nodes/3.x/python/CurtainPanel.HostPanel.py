9ae508c09dbceb362daace70c214d67dcbde7ce8
Andreas Dieckmann <andreas@badmonkeys.net>
2025-04-19 11:35:52 +0200

    fixed #457

--- nodes/3.x/python/CurtainPanel.HostPanel.py
+++ nodes/3.x/python/CurtainPanel.HostPanel.py
@@ -7,13 +7,16 @@ import Revit
 clr.ImportExtensions(Revit.Elements)
 
 def GetHostPanel(item):
-	if hasattr(item, "FindHostPanel"):
-		hpId = item.FindHostPanel()
-		if hpId.IntegerValue > 0: return item.Document.GetElement(hpId).ToDSType(True)
-		else: return item
-	else: return item
+    if hasattr(item, "FindHostPanel"):
+        hpId = item.FindHostPanel()
+        if version > 2024: hpIdVal = hpId.Value
+        else: hpIdVal = hpId.IntegerValue
+        if hpIdVal > 0: return item.Document.GetElement(hpId).ToDSType(True)
+        else: return item
+    else: return item
 
 items = UnwrapElement(IN[0])
+version = IN[1]
 
 if isinstance(IN[0], list): OUT = [GetHostPanel(x) for x in items]
 else: OUT = GetHostPanel(items)
