0e0b7b3d6e127907f78eb455f023a8a6bf46a1fb
Andreas Dieckmann <andreas@badmonkeys.net>
2025-04-20 00:19:59 +0200

    fixed #476

--- nodes/3.x/python/Element.ID+.py
+++ nodes/3.x/python/Element.ID+.py
@@ -7,10 +7,13 @@ import Revit
 clr.ImportExtensions(Revit.Elements)
 
 def GetId(item):
-	if hasattr(item, "Id"): return item.Id.IntegerValue
-	else: return None
+    if hasattr(item, "Id"): 
+        if version > 2024: return item.Id.Value
+        else: return item.Id.IntegerValue
+    else: return None
 
 items = UnwrapElement(IN[0])
+version = IN[1]
 
 if isinstance(IN[0], list): OUT = [GetId(x) for x in items]
 else: OUT = GetId(items)
