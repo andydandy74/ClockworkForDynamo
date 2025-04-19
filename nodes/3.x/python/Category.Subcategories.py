1c52622bb9f760c781de230786fb4d232c518d88
Andreas Dieckmann <andreas@badmonkeys.net>
2025-04-19 10:18:35 +0200

    fixed #462

--- nodes/3.x/python/Category.Subcategories.py
+++ nodes/3.x/python/Category.Subcategories.py
@@ -7,11 +7,13 @@ import Revit
 clr.ImportExtensions(Revit.Elements)
 
 def GetSubcategories(cat):
-	if hasattr(cat, "SubCategories"):
-		return [Revit.Elements.Category.ById(x.Id.IntegerValue) for x in cat.SubCategories]
-	else: return []
+    if hasattr(cat, "SubCategories"):
+        if version > 2025: return [Revit.Elements.Category.ById(x.Id.Value) for x in cat.SubCategories]
+        else: return [Revit.Elements.Category.ById(x.Id.IntegerValue) for x in cat.SubCategories]
+    else: return []
 
 cats = UnwrapElement(IN[0])
+version = IN[1]
 
 if isinstance(IN[0], list): OUT = [GetSubcategories(x) for x in cats]
 else: OUT = GetSubcategories(cats)
