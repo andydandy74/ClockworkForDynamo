beaade56abf7a265483f18cf8af04692be5d8cd1
Andreas Dieckmann <andreas@badmonkeys.net>
2025-04-19 15:43:58 +0200

    fixed #458

--- nodes/3.x/python/FilledRegionType.FillPatterns.py
+++ nodes/3.x/python/FilledRegionType.FillPatterns.py
@@ -6,20 +6,13 @@ def GetFillPatterns(frt):
     foreground = None
     background = None
     if hasattr(frt, "ForegroundPatternId"):
-        if version > 2024:
-            if frt.ForegroundPatternId.Value > 0:
-                foreground = frt.Document.GetElement(frt.ForegroundPatternId)
-            if frt.BackgroundPatternId.Value > 0:
-                background = frt.Document.GetElement(frt.BackgroundPatternId)
-        else:
-            if frt.ForegroundPatternId.IntegerValue > 0:
-                foreground = frt.Document.GetElement(frt.ForegroundPatternId)
-            if frt.BackgroundPatternId.IntegerValue > 0:
-                background = frt.Document.GetElement(frt.BackgroundPatternId)
+        if frt.ForegroundPatternId != ElementId.InvalidElementId:
+            foreground = frt.Document.GetElement(frt.ForegroundPatternId)
+        if frt.BackgroundPatternId != ElementId.InvalidElementId:
+            background = frt.Document.GetElement(frt.BackgroundPatternId)
     return foreground, background
 
 filledregiontypes = UnwrapElement(IN[0])
-version = IN[1]
 
 if isinstance(IN[0], list): OUT = list(map(list, zip(*[GetFillPatterns(x) for x in filledregiontypes])))
 else: OUT = GetFillPatterns(filledregiontypes)
