eb9f79a1a4e6de07404aa9985c96147d0608130b
Andreas Dieckmann <andreas@badmonkeys.net>
2025-04-20 00:30:06 +0200

    fixed #474

--- nodes/3.x/python/Element.Category+.py
+++ nodes/3.x/python/Element.Category+.py
@@ -8,45 +8,50 @@ import Revit
 clr.ImportExtensions(Revit.Elements)
 
 def GetCategory(item):
-	if not item: return None, None
-	objtype = None
-	if hasattr(item, "GetType"): objtype = item.GetType().ToString()
-	returnID = None
-	returnIDs = None
-	returnCat = None
-	returnBic = None
-	if objtype == "Autodesk.Revit.DB.ParameterFilterElement": returnIDs = item.GetCategories()
-	elif objtype == "Autodesk.Revit.DB.ViewSchedule": returnID = item.Definition.CategoryId
-	elif objtype == "Autodesk.Revit.DB.Family": returnID = item.FamilyCategoryId
-	elif objtype == "Autodesk.Revit.DB.ColorFillScheme": returnID = item.CategoryId
-	elif objtype == "Autodesk.Revit.DB.GraphicsStyle":  returnID = item.GraphicsStyleCategory.Id
-	elif objtype == "Revit.Application.Document":
-		if item.IsFamilyDocument: 
-			clr.AddReference("RevitServices")
-			import RevitServices
-			from RevitServices.Persistence import DocumentManager
-			returnID = DocumentManager.Instance.CurrentDBDocument.OwnerFamily.FamilyCategoryId
-	elif objtype == "Autodesk.Revit.DB.Category": 
-		if item.Parent: returnID = item.Parent.Id
-	elif hasattr(item, "Category"): 
-		if item.Category: returnID = item.Category.Id
-	if returnID:
-		returnBic = System.Enum.GetName(BuiltInCategory, returnID.IntegerValue)
-		try: returnCat =  Revit.Elements.Category.ById(returnID.IntegerValue)
-		except: pass
-	elif returnIDs:
-		returnCat = []
-		returnBic = []
-		for returnID in returnIDs:
-			returnBic.append(System.Enum.GetName(BuiltInCategory, returnID.IntegerValue))
-			try: returnCat.append(Revit.Elements.Category.ById(returnID.IntegerValue))
-			except: returnCat.append(None)
-		returnCat.sort()
-	return returnCat, returnBic
+    if not item: return None, None
+    objtype = None
+    if hasattr(item, "GetType"): objtype = item.GetType().ToString()
+    returnID = None
+    returnIDs = None
+    returnCat = None
+    returnBic = None
+    if objtype == "Autodesk.Revit.DB.ParameterFilterElement": returnIDs = item.GetCategories()
+    elif objtype == "Autodesk.Revit.DB.ViewSchedule": returnID = item.Definition.CategoryId
+    elif objtype == "Autodesk.Revit.DB.Family": returnID = item.FamilyCategoryId
+    elif objtype == "Autodesk.Revit.DB.ColorFillScheme": returnID = item.CategoryId
+    elif objtype == "Autodesk.Revit.DB.GraphicsStyle":  returnID = item.GraphicsStyleCategory.Id
+    elif objtype == "Revit.Application.Document":
+        if item.IsFamilyDocument: 
+            clr.AddReference("RevitServices")
+            import RevitServices
+            from RevitServices.Persistence import DocumentManager
+            returnID = DocumentManager.Instance.CurrentDBDocument.OwnerFamily.FamilyCategoryId
+    elif objtype == "Autodesk.Revit.DB.Category": 
+        if item.Parent: returnID = item.Parent.Id
+    elif hasattr(item, "Category"): 
+        if item.Category: returnID = item.Category.Id
+    if returnID:
+        if version > 2024: returnIDval = returnID.Value
+        else: returnIDval = returnID.IntegerValue
+        returnBic = System.Enum.GetName(BuiltInCategory, returnIDval)
+        try: returnCat =  Revit.Elements.Category.ById(returnIDval)
+        except: pass
+    elif returnIDs:
+        returnCat = []
+        returnBic = []
+        for returnID in returnIDs:
+            if version > 2024: returnIDval = returnID.Value
+            else: returnIDval = returnID.IntegerValue
+            returnBic.append(System.Enum.GetName(BuiltInCategory, returnIDval))
+            try: returnCat.append(Revit.Elements.Category.ById(returnIDval))
+            except: returnCat.append(None)
+        returnCat.sort()
+    return returnCat, returnBic
 
 items = UnwrapElement(IN[0])
+version = IN[1]
 
 if isinstance(IN[0], list): 
-	if len(IN[0]) > 0: OUT = map(list, zip(*[GetCategory(x) for x in items]))
-	else: OUT = [[],[]]
+    if len(IN[0]) > 0: OUT = map(list, zip(*[GetCategory(x) for x in items]))
+    else: OUT = [[],[]]
 else: OUT = GetCategory(items)
