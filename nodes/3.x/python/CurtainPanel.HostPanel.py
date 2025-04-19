45379e53397837f3c34801bc8a9cef12deef16af
Andreas Dieckmann <andreas@badmonkeys.net>
2025-04-19 19:08:14 +0200

    fixed #457

--- nodes/3.x/python/CurtainPanel.HostPanel.py
+++ nodes/3.x/python/CurtainPanel.HostPanel.py
@@ -9,14 +9,11 @@ clr.ImportExtensions(Revit.Elements)
 def GetHostPanel(item):
     if hasattr(item, "FindHostPanel"):
         hpId = item.FindHostPanel()
-        if version > 2024: hpIdVal = hpId.Value
-        else: hpIdVal = hpId.IntegerValue
-        if hpIdVal > 0: return item.Document.GetElement(hpId).ToDSType(True)
+        if hpId != ElementId.InvalidElementId: return item.Document.GetElement(hpId).ToDSType(True)
         else: return item
     else: return item
 
 items = UnwrapElement(IN[0])
-version = IN[1]
 
 if isinstance(IN[0], list): OUT = [GetHostPanel(x) for x in items]
 else: OUT = GetHostPanel(items)
