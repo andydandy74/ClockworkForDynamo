c3b2b3eba4361555d6c81b7ebb91845f7b0aeadf
Andreas Dieckmann <andreas@badmonkeys.net>
2025-04-19 11:10:43 +0200

    straggler for #417, fixed #458

--- nodes/3.x/python/FilledRegionType.FillPatterns.py
+++ nodes/3.x/python/FilledRegionType.FillPatterns.py
@@ -3,19 +3,23 @@ clr.AddReference('RevitAPI')
 from Autodesk.Revit.DB import *
 
 def GetFillPatterns(frt):
-	foreground = None
-	background = None
-	if hasattr(frt, "ForegroundPatternId"):
-		if frt.ForegroundPatternId.IntegerValue > 0:
-			foreground = frt.Document.GetElement(frt.ForegroundPatternId)
-		if frt.BackgroundPatternId.IntegerValue > 0:
-			background = frt.Document.GetElement(frt.BackgroundPatternId)
-	elif hasattr(frt, "FillPatternId"):	
-		if frt.FillPatternId.IntegerValue > 0:
-			foreground = frt.Document.GetElement(frt.FillPatternId)
-	return foreground, background
+    foreground = None
+    background = None
+    if hasattr(frt, "ForegroundPatternId"):
+        if version > 2024:
+            if frt.ForegroundPatternId.Value > 0:
+                foreground = frt.Document.GetElement(frt.ForegroundPatternId)
+            if frt.BackgroundPatternId.Value > 0:
+                background = frt.Document.GetElement(frt.BackgroundPatternId)
+        else:
+            if frt.ForegroundPatternId.IntegerValue > 0:
+                foreground = frt.Document.GetElement(frt.ForegroundPatternId)
+            if frt.BackgroundPatternId.IntegerValue > 0:
+                background = frt.Document.GetElement(frt.BackgroundPatternId)
+    return foreground, background
 
 filledregiontypes = UnwrapElement(IN[0])
+version = IN[1]
 
 if isinstance(IN[0], list): OUT = list(map(list, zip(*[GetFillPatterns(x) for x in filledregiontypes])))
 else: OUT = GetFillPatterns(filledregiontypes)
