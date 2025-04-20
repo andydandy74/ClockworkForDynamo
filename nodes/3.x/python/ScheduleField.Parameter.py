02780db54931db70d3e13b8119181822762a1058
Andreas Dieckmann <andreas@badmonkeys.net>
2025-04-20 13:46:43 +0200

    fixed #468

--- nodes/3.x/python/ScheduleField.Parameter.py
+++ nodes/3.x/python/ScheduleField.Parameter.py
@@ -4,19 +4,22 @@ clr.AddReference('RevitAPI')
 from Autodesk.Revit.DB import *
 
 def GetScheduleFieldParam(schedulefield):
-	if hasattr(schedulefield, "ParameterId"): 
-		pId = schedulefield.ParameterId
-		if pId != ElementId.InvalidElementId: 
-			# BuiltInParameters
-			if pId.IntegerValue < 0: 
-				try: return System.Enum.GetName(BuiltInParameter, pId.IntegerValue)
-				except: return None
-			# Project/Family/Shared Parameters
-			else: return schedulefield.Schedule.Document.GetElement(schedulefield.ParameterId)
-		else: return None
-	else: return None
+    if hasattr(schedulefield, "ParameterId"): 
+        pId = schedulefield.ParameterId
+        if pId != ElementId.InvalidElementId: 
+            if version > 2024: pIdVal = pId.Value
+            else: pIdVal = pId.IntegerValue
+            # BuiltInParameters
+            if pIdVal < 0: 
+                try: return System.Enum.GetName(BuiltInParameter, pIdVal)
+                except: return None
+            # Project/Family/Shared Parameters
+            else: return schedulefield.Schedule.Document.GetElement(schedulefield.ParameterId)
+        else: return None
+    else: return None
 
 schedulefields = UnwrapElement(IN[0])
+version = IN[1]
 
 if isinstance(IN[0], list): OUT = [GetScheduleFieldParam(x) for x in schedulefields]
 else: OUT = GetScheduleFieldParam(schedulefields)
