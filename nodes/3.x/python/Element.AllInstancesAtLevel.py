4f4a8e1ab28816e2e07e0e5ea4ed5b04dc93a8e2
Andreas Dieckmann <andreas@badmonkeys.net>
2025-04-21 16:10:20 +0200

    fixed #473

--- nodes/3.x/python/Element.AllInstancesAtLevel.py
+++ nodes/3.x/python/Element.AllInstancesAtLevel.py
@@ -4,17 +4,21 @@ clr.AddReference('RevitAPI')
 from Autodesk.Revit.DB import *
 
 def GetAllInstancesAtLevel(item, lvl):
-	collector = FilteredElementCollector(item.Document)
-	bic = System.Enum.ToObject(BuiltInCategory, item.Category.Id.IntegerValue)
-	lvlfilter = ElementLevelFilter(lvl.Id)
-	collector.OfCategory(bic).WherePasses(lvlfilter)
-	# This is a workaround
-	# because I was too lazy to learn
-	# how to implement LINQ in Python
-	return [x for x in collector.ToElements() if x.GetTypeId().IntegerValue == item.GetTypeId().IntegerValue]
+    collector = FilteredElementCollector(item.Document)
+    if version > 2024: catID = item.Category.Id.Value
+    else: catID = item.Category.Id.IntegerValue
+    bic = System.Enum.ToObject(BuiltInCategory, catID)
+    lvlfilter = ElementLevelFilter(lvl.Id)
+    collector.OfCategory(bic).WherePasses(lvlfilter)
+    # This is a workaround
+    # because I was too lazy to learn
+    # how to implement LINQ in Python
+    if version > 2024: return [x for x in collector.ToElements() if x.GetTypeId().Value == item.GetTypeId().Value]
+    else: return [x for x in collector.ToElements() if x.GetTypeId().IntegerValue == item.GetTypeId().IntegerValue]
 
 elements = UnwrapElement(IN[0])
 lvl = UnwrapElement(IN[1])
+version = IN[2]
 
 if isinstance(IN[0], list): OUT = [GetAllInstancesAtLevel(x, lvl) for x in elements]
 else: OUT = GetAllInstancesAtLevel(elements, lvl)
