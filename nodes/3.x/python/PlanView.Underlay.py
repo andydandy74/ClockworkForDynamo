6618f345b003370029a90b7120e7820a81d93eb6
Andreas Dieckmann <andreas@badmonkeys.net>
2025-04-21 10:28:39 +0200

    fixed #469

--- nodes/3.x/python/PlanView.Underlay.py
+++ nodes/3.x/python/PlanView.Underlay.py
@@ -4,14 +4,14 @@ from Autodesk.Revit.DB import *
 import System
 
 def GetUnderlays(view):
-	baselvl = None
-	toplvl = None
-	ulOrientation = None
-	if hasattr(view, "GetUnderlayBaseLevel"):	
-		if view.GetUnderlayBaseLevel().IntegerValue > 0: baselvl = view.Document.GetElement(view.GetUnderlayBaseLevel())
-		if view.GetUnderlayTopLevel().IntegerValue > 0: toplvl = view.Document.GetElement(view.GetUnderlayTopLevel())
-		ulOrientation = System.Enum.GetName(UnderlayOrientation, view.GetUnderlayOrientation())
-	return baselvl, toplvl, ulOrientation
+    baselvl = None
+    toplvl = None
+    ulOrientation = None
+    if hasattr(view, "GetUnderlayBaseLevel"):    
+        if view.GetUnderlayBaseLevel() != ElementId.InvalidElementId: baselvl = view.Document.GetElement(view.GetUnderlayBaseLevel())
+        if view.GetUnderlayTopLevel() != ElementId.InvalidElementId: toplvl = view.Document.GetElement(view.GetUnderlayTopLevel())
+        ulOrientation = System.Enum.GetName(UnderlayOrientation, view.GetUnderlayOrientation())
+    return baselvl, toplvl, ulOrientation
 
 views = UnwrapElement(IN[0])
 
