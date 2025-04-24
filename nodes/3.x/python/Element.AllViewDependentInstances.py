e05720446b3842036fb6ed2d6764ee4447669668
Andreas Dieckmann <andreas@badmonkeys.net>
2025-04-24 08:05:52 +0200

    fixed #471

--- nodes/3.x/python/Element.AllViewDependentInstances.py
+++ nodes/3.x/python/Element.AllViewDependentInstances.py
@@ -4,17 +4,21 @@ clr.AddReference('RevitAPI')
 from Autodesk.Revit.DB import *
 
 def GetAllInstancesInView(item, view):
-	collector = FilteredElementCollector(item.Document)
-	bic = System.Enum.ToObject(BuiltInCategory, item.Category.Id.IntegerValue)
-	viewfilter = ElementOwnerViewFilter(view.Id)
-	collector.WherePasses(viewfilter).OfCategory(bic)
-	# This is a workaround
-	# because I was too lazy to learn
-	# how to implement LINQ in Python
-	return [x for x in collector.ToElements() if x.GetTypeId().IntegerValue == item.GetTypeId().IntegerValue]
+    collector = FilteredElementCollector(item.Document)
+    if version > 2024: catID = item.Category.Id.Value
+    else: item.Category.Id.IntegerValue
+    bic = System.Enum.ToObject(BuiltInCategory, catID)
+    viewfilter = ElementOwnerViewFilter(view.Id)
+    collector.WherePasses(viewfilter).OfCategory(bic)
+    # This is a workaround
+    # because I was too lazy to learn
+    # how to implement LINQ in Python
+    if version > 2024: return [x for x in collector.ToElements() if x.GetTypeId().Value == item.GetTypeId().Value]
+    else: return [x for x in collector.ToElements() if x.GetTypeId().IntegerValue == item.GetTypeId().IntegerValue]
 
 elements = UnwrapElement(IN[0])
 view = UnwrapElement(IN[1])
+version = IN[2]
 
 if isinstance(IN[0], list): OUT = [GetAllInstancesInView(x, view) for x in elements]
 else: OUT = GetAllInstancesInView(elements, view)
