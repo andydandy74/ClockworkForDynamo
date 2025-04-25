1b9122caa20b32c0206dfc1ca519a5dbab920df9
Andreas Dieckmann <andreas@badmonkeys.net>
2025-04-25 10:37:39 +0200

    fixed #448

--- nodes/2.x/python/Color.Name.py
+++ nodes/2.x/python/Color.Name.py
@@ -4,13 +4,14 @@ import System.Drawing
 
 def GetColorName(color):
 	if hasattr(color, "Red"):
-		if color.Red == 0 and color.Green == 0 and color.Blue == 0: return "Black"
-		else:
-			lookup = [x for x in knowncols if x.R == color.Red and x.G == color.Green and x.B == color.Blue]
-			if len(lookup) > 0: return lookup[0].Name
-			else: return None
+		lookup = [x for x in syscols if x.R == color.Red and x.G == color.Green and x.B == color.Blue]
+		if len(lookup) > 0: return lookup[0].Name
+		else: return None
 	else: return None
 
-knowncols = [System.Drawing.Color.FromKnownColor(x) for x in System.Enum.GetValues(System.Drawing.KnownColor)]
+syscols = []
+syscols.append(System.Drawing.Color.FromName("Black"))
+syscols.append(System.Drawing.Color.FromName("White"))
+syscols.extend([System.Drawing.Color.FromKnownColor(x) for x in System.Enum.GetValues(System.Drawing.KnownColor)])
 if isinstance(IN[0], list): OUT = [GetColorName(x) for x in IN[0]]
 else: OUT = GetColorName(IN[0])
