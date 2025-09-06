d68caa50011cf2eb53c20e713324d4ca3446d519
Andreas Dieckmann <andreas@badmonkeys.net>
2025-09-06 11:03:17 +0200

    #489

--- nodes/2.x/python/Array.AssociatedFamilyParameter.py
+++ nodes/2.x/python/Array.AssociatedFamilyParameter.py
@@ -7,12 +7,14 @@ import Revit
 clr.ImportExtensions(Revit.Elements)
 
 def GetAssociatedFamilyParameters(basearray):
-	if basearray.GetType().ToString() in ("Autodesk.Revit.DB.LinearArray", "Autodesk.Revit.DB.RadialArray"):
-		if basearray.Label != None: return basearray.Label, basearray.Label.Definition.Name
-		else: return None, None
-	else: return None, None
+    if basearray.GetType().ToString() in ("Autodesk.Revit.DB.LinearArray", "Autodesk.Revit.DB.RadialArray"):
+        if basearray.Label != None: return basearray.Label, basearray.Label.Definition.Name
+        else: return None, None
+    else: return None, None
 
 basearrays = UnwrapElement(IN[0])
 
-if isinstance(IN[0], list): OUT = map(list, zip(*[GetAssociatedFamilyParameters(x) for x in basearrays]))
+if isinstance(IN[0], list): 
+    if len(IN[0]) > 0: OUT = map(list, zip(*[GetAssociatedFamilyParameters(x) for x in basearrays]))
+    else: OUT = [[],[]]
 else: OUT = GetAssociatedFamilyParameters(basearrays)
