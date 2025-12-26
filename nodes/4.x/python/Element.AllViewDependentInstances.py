44227312f940891114bcd57da156c7760e6c917b
Andreas Dieckmann <andreas@badmonkeys.net>
2025-12-26 11:08:35 +0100

    #493

--- nodes/4.x/python/Element.AllViewDependentInstances.py
+++ nodes/4.x/python/Element.AllViewDependentInstances.py
@@ -5,20 +5,14 @@ from Autodesk.Revit.DB import *
 
 def GetAllInstancesInView(item, view):
     collector = FilteredElementCollector(item.Document)
-    if version > 2024: catID = item.Category.Id.Value
-    else: catID = item.Category.Id.IntegerValue
+    catID = item.Category.Id.Value
     bic = System.Enum.ToObject(BuiltInCategory, catID)
     viewfilter = ElementOwnerViewFilter(view.Id)
     collector.WherePasses(viewfilter).OfCategory(bic)
-    # This is a workaround
-    # because I was too lazy to learn
-    # how to implement LINQ in Python
-    if version > 2024: return [x for x in collector.ToElements() if x.GetTypeId().Value == item.GetTypeId().Value]
-    else: return [x for x in collector.ToElements() if x.GetTypeId().IntegerValue == item.GetTypeId().IntegerValue]
+    return [x for x in collector.ToElements() if x.GetTypeId().Value == item.GetTypeId().Value]
 
 elements = UnwrapElement(IN[0])
 view = UnwrapElement(IN[1])
-version = IN[2]
 
 if isinstance(IN[0], list): OUT = [GetAllInstancesInView(x, view) for x in elements]
 else: OUT = GetAllInstancesInView(elements, view)
