1ad4bc92b17457364f492e3d35350f5d9cdf3cd1
Andreas Dieckmann <andreas@badmonkeys.net>
2025-12-26 09:34:54 +0100

    #493

--- nodes/4.x/python/All Elements of Family Type+.py
+++ nodes/4.x/python/All Elements of Family Type+.py
@@ -10,17 +10,14 @@ from RevitServices.Persistence import DocumentManager
 def InstancesOfFamilyType(ft, doc):
     collector = FilteredElementCollector(doc)
     try:
-        if version > 2024: ftCat = ft.Category.Id.Value
-        else: ftCat = ft.Category.Id.IntegerValue
+        ftCat = ft.Category.Id.Value
         bic = System.Enum.ToObject(BuiltInCategory, ftCat)
         collector.OfCategory(bic)
-        if version > 2024: return [x for x in collector.ToElements() if x.GetTypeId().Value == ft.Id.Value]
-        else: return [x for x in collector.ToElements() if x.GetTypeId().IntegerValue == ft.Id.IntegerValue]
+        return [x for x in collector.ToElements() if x.GetTypeId().Value == ft.Id.Value]
     except: return []
 
 famtypes = UnwrapElement(IN[0])
 inputdoc = UnwrapElement(IN[2])
-version = IN[3]
 if not inputdoc: doc = DocumentManager.Instance.CurrentDBDocument
 elif inputdoc.GetType().ToString() == "Autodesk.Revit.DB.RevitLinkInstance": doc = inputdoc.GetLinkDocument()
 elif inputdoc.GetType().ToString() == "Autodesk.Revit.DB.Document": doc = inputdoc
