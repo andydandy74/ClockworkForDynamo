aa33fae635e81242c9236865fcb67b12eaf3b169
Andreas Dieckmann <andreas@badmonkeys.net>
2026-01-24 22:16:19 +0100

    fixed #503 (ported to 4.x)

--- nodes/4.x/python/FamilyType.Duplicate.py
+++ nodes/4.x/python/FamilyType.Duplicate.py
@@ -12,24 +12,24 @@ from RevitServices.Persistence import DocumentManager
 from RevitServices.Transactions import TransactionManager
 
 def DuplicateFamType(famtype, name):
-	try:
-		return famtype.Duplicate(name)
-	except:
-		try:
-			t1 = famtype.GetType().ToString()
-			if t1 in func_enum: return func_enum[t1](name)
-			else: return None
-		except:
-			return None
+    try:
+        return famtype.Duplicate(name)
+    except:
+        try:
+            t1 = famtype.GetType().ToString()
+            if t1 in func_enum: return func_enum[t1](name)
+            else: return None
+        except:
+            return None
 
 doc = DocumentManager.Instance.CurrentDBDocument
 famtypes = IN[0]
 newnames = IN[1]
 func_enum = {'Revit.Elements.WallType' : Revit.Elements.WallType.ByName,
-			 'Revit.Elements.FloorType' : Revit.Elements.FloorType.ByName,
-			 'Revit.Elements.FamilyType' : Revit.Elements.FamilyType.ByName}
+             'Revit.Elements.FloorType' : Revit.Elements.FloorType.ByName,
+             'Revit.Elements.FamilyType' : Revit.Elements.FamilyType.ByName}
 
 TransactionManager.Instance.EnsureInTransaction(doc)
-if isinstance(famtypes, list): OUT = [DuplicateFamType(x, y) for x, y in zip(famtypes, newnames)]
-else: OUT = DuplicateFamType(famtypes, newnames)
+if isinstance(famtypes, list): OUT = UnwrapElement([DuplicateFamType(x, y) for x, y in zip(famtypes, newnames)])
+else: OUT = UnwrapElement(DuplicateFamType(famtypes, newnames))
 TransactionManager.Instance.TransactionTaskDone()
